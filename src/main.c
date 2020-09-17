b35b1ad06f497cad9498706e159fafe8d003e339
windlunar <x810041@gmail.com>
2020-09-18 02:57:55 +0800

    Test the print function

--- src/main.c
+++ src/main.c
@@ -8,11 +8,12 @@
 
 int _main (void)
 {
-	char testvar = 0x41 ;
+	char testvar1 = 'A' ;
+	char testvar2 = 'B' ;
+	char testvar3 = 'C' ;
 	usrLed_Init() ;
 	uart_tx_str(UART0_PTR ,"\nWelcome to my very first Beaglebone OS Kernal!\r\n" ,49) ;
-	terminal_printC(0x40) ;
-	kprintf("print testvar = %c\r\n" ,testvar) ;
+	kprintf("print testvar1 = %c, testvar2 = %c, testvar3 = %c\r\n" ,testvar1,testvar2,testvar3) ;
 	while(1){
 		usrLed_blink() ;
 	}
