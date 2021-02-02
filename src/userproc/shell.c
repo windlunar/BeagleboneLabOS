025991e243260add656cf6c117266fb63d74a44b
user <x810041@gmail.com>
2021-02-02 22:23:12 +0800

    Fix backspace bug in cli

--- src/userproc/shell.c
+++ src/userproc/shell.c
@@ -42,6 +42,7 @@ void main_shell()
 	char byte = '\0' ;
 	char cmdbuf[SIZE_OF_CMDBUF] ;
 	_memset((void *)cmdbuf ,0 ,SIZE_OF_CMDBUF) ;
+
 	char *cmd = cmdbuf ;
 	char *s = cmd;
 
@@ -58,7 +59,7 @@ void main_shell()
 		//在console上印出輸入的字元
 		uprintf("%c" ,byte) ;
 		
-		if(_strlen(cmd) == sizeof(cmdbuf)-1)
+		if(_strlen(cmd) == sizeof(cmdbuf)-2)
 		{
 			_memset((void *)cmdbuf ,0 ,SIZE_OF_CMDBUF) ;
 			s = cmd ;
@@ -71,119 +72,120 @@ void main_shell()
 
 		switch (byte)
 		{
-		case 0x08 : //Backspace
-			put_c(' ') ;
-			put_c(0x08) ;
-			_memset((void *)cmdbuf ,0 ,SIZE_OF_CMDBUF) ;
-			s = cmd ;
-			break;
-		
-		case 0x0d:	//Enter ,按下Enter鍵後的處理
-			_memset(token ,0 ,SIZE_OF_CMD_TOKEN) ;
-			_strcat(cmd ,delim) ;	//這行不加的話 type cd指令會出錯
-			cmd = strtok_fst(cmd ,delim ,_strlen(delim) ,token) ;
-
-			if (_strcmp(token ,"test\0")==0){
-				uprintf("\r\n");
-				uprintf("Please key\r\n");
-				uprintf("	'1' for fork function test.\r\n") ;
-				uprintf("	'2' for multitasking test\r\n") ; 
-				uprintf("	'3' for priority-based multitasking test.\r\n") ;
-				uprintf("	'4' for tty0 open test.\r\n") ;
-				uprintf("Or '0' break.\r\n") ;
-
-				while(1)
-				{
-					//獲取在console輸入的字元
-					uint8_t get = get_c() ;
-
-					//在console上印出輸入的字元
-					uprintf("%c" ,get) ;
-					
-					if(get == '1'){
-						// Test Fork function
-						fork_test() ;
-						for(int i = 0 ; i<20000;i++) ;
-						break ;
-
-					}else if(get == '2'){
-						// 測試 multitasking							
-						// Time slice base. Round Robin Multitasking    
-						multitasking_test_main() ;
-						for(int i = 0 ; i<20000;i++) ;
-						break ;
-
-					}else if(get == '3'){
-						// 測試 priority							
-						// Time slice base. priority base Multitasking    
-						priority_test_main() ;
-						for(int i = 0 ; i<500000;i++) ;
-						break ;
-
-					}else if(get == '4'){
-						// 測試 tty0							
-						tty0_test_main() ;
-						for(int i = 0 ; i<20000;i++) ;
-						break ;
-
-					}else if(get == '0'){
-						break ;
-					}
-				}
-
-			}else if(_strcmp(token ,"help\0")==0){
-				uprintf("\r\n");
-				uprintf("help:\r\n") ;
-				uprintf("Available command :test ,info ,help\r\n") ;
-
-			}else if(_strcmp(token ,"info\0")==0){
-				uprintf("\r\n");
-				uprintf("A simple os running on Beaglebone black\r\n\r\n") ;
-				uprintf("Feature:\r\n") ;
-				uprintf("  Time slice based round-robin multitasking.\r\n");
-				uprintf("  Simple memory management.\r\n");
-				uprintf("  Simple command line.\r\n");
-
-			// command 'ls'
-			}else if(_strcmp(token ,"ls\0")==0){
-				lsdir() ;
-				put_str("\r\n\0") ;
-
-			// command 'cd'
-			}else if(_strcmp(token ,"cd\0")==0){
+			case 0x08 : //Backspace
+				put_c(' ') ;
+				put_c(0x08) ;
+				if(s != cmdbuf) s-- ;
+				*s = '\0' ;
+				
+				break;
+			
+			case 0x0d:	//Enter ,按下Enter鍵後的處理
 				_memset(token ,0 ,SIZE_OF_CMD_TOKEN) ;
-				if(*cmd == '\0'){
-					put_str("No dir input\r\n") ;
-				}else{
-					cmd = strtok_fst(cmd ,delim ,_strlen(delim) ,token) ;
+				_strcat(cmd ,delim) ;	//這行不加的話 type cd指令會出錯
+				cmd = strtok_fst(cmd ,delim ,_strlen(delim) ,token) ;
+
+				if (_strcmp(token ,"test\0")==0){
+					uprintf("\r\n");
+					uprintf("Please key\r\n");
+					uprintf("	'1' for fork function test.\r\n") ;
+					uprintf("	'2' for multitasking test\r\n") ; 
+					uprintf("	'3' for priority-based multitasking test.\r\n") ;
+					uprintf("	'4' for tty0 open test.\r\n") ;
+					uprintf("Or '0' break.\r\n") ;
+
+					while(1)
+					{
+						//獲取在console輸入的字元
+						uint8_t get = get_c() ;
+
+						//在console上印出輸入的字元
+						uprintf("%c" ,get) ;
+						
+						if(get == '1'){
+							// Test Fork function
+							fork_test() ;
+							for(int i = 0 ; i<20000;i++) ;
+							break ;
+
+						}else if(get == '2'){
+							// 測試 multitasking							
+							// Time slice base. Round Robin Multitasking    
+							multitasking_test_main() ;
+							for(int i = 0 ; i<20000;i++) ;
+							break ;
+
+						}else if(get == '3'){
+							// 測試 priority							
+							// Time slice base. priority base Multitasking    
+							priority_test_main() ;
+							for(int i = 0 ; i<500000;i++) ;
+							break ;
+
+						}else if(get == '4'){
+							// 測試 tty0							
+							tty0_test_main() ;
+							for(int i = 0 ; i<20000;i++) ;
+							break ;
+
+						}else if(get == '0'){
+							break ;
+						}
+					}
+
+				}else if(_strcmp(token ,"help\0")==0){
+					uprintf("\r\n");
+					uprintf("help:\r\n") ;
+					uprintf("Available command :test ,info ,help\r\n") ;
+
+				}else if(_strcmp(token ,"info\0")==0){
+					uprintf("\r\n");
+					uprintf("A simple os running on Beaglebone black\r\n\r\n") ;
+					uprintf("Feature:\r\n") ;
+					uprintf("  Time slice based round-robin multitasking.\r\n");
+					uprintf("  Simple memory management.\r\n");
+					uprintf("  Simple command line.\r\n");
+
+				// command 'ls'
+				}else if(_strcmp(token ,"ls\0")==0){
+					lsdir() ;
+					put_str("\r\n\0") ;
+
+				// command 'cd'
+				}else if(_strcmp(token ,"cd\0")==0){
+					_memset(token ,0 ,SIZE_OF_CMD_TOKEN) ;
+					if(*cmd == '\0'){
+						put_str("No dir input\r\n") ;
+					}else{
+						cmd = strtok_fst(cmd ,delim ,_strlen(delim) ,token) ;
+					}
+
+					if(cd(token) < 0) put_str("\r\nDir not found") ;
+					put_str("\r\n\0") ;
+
+				// command 'pwd'	
+				}else if(_strcmp(token ,"pwd\0")==0){
+					_memset(cwd ,0 ,SIZE_OF_CWD) ;
+					pwd(cwd ,SIZE_OF_CWD) ;
+					put_str("\r\n\0") ;
+					put_str(cwd);
+					put_str("\r\n\0") ;
 				}
 
-				if(cd(token) < 0) put_str("\r\nDir not found") ;
-				put_str("\r\n\0") ;
+				_memset((void *)cmdbuf ,0 ,SIZE_OF_CMDBUF) ;
+				cmd = cmdbuf ;
+				s = cmd ;
 
-			// command 'pwd'	
-			}else if(_strcmp(token ,"pwd\0")==0){
+				put_str("\r\n|cmd>\0") ;
 				_memset(cwd ,0 ,SIZE_OF_CWD) ;
 				pwd(cwd ,SIZE_OF_CWD) ;
-				put_str("\r\n\0") ;
 				put_str(cwd);
-				put_str("\r\n\0") ;
-			}
-
-			_memset((void *)cmdbuf ,0 ,SIZE_OF_CMDBUF) ;
-			cmd = cmdbuf ;
-			s = cmd ;
-
-			put_str("\r\n|cmd>\0") ;
-			_memset(cwd ,0 ,SIZE_OF_CWD) ;
-			pwd(cwd ,SIZE_OF_CWD) ;
-			put_str(cwd);
-			put_str(">\0") ;
+				put_str(">\0") ;
+				
+				break;
 			
-			break;
-		
-		default:
-			break;
+			default:
+				break;
 		}
 	}
 	
